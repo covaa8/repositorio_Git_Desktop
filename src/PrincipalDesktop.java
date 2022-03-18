448416e199a10b414be5af089406120a0d952127
cova <covadongabustantigo@gmail.com>
2022-03-18 21:09:10 +0100

    Primera actualización Git Desktop

---
+++ src/PrincipalDesktop.java
@@ -0,0 +1,9 @@
+
+public class PrincipalDesktop {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		System.out.println("Modificacion hecha en el proyecto");
+	}
+
+}
