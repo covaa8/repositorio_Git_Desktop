936cc375036fc7925ea603c414ff5375e29446eb
cova <covadongabustantigo@gmail.com>
2022-03-19 17:00:46 +0100

    Segunda Confirmación Desktop

--- src/PrincipalDesktop.java
+++ src/PrincipalDesktop.java
@@ -4,6 +4,7 @@ public class PrincipalDesktop {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Modificacion hecha en el proyecto");
+		System.out.println("Segunda modificacion hecha en el proyecto");
 	}
 
 }
